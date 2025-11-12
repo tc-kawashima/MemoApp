307e7851701a4807d4134fbdbcacd38bb82e2c97
tc-kawashima <y.kawashima@timecapsuleinc.co.jp>
2025-11-12 13:40:24 +0900

    .env

---
+++ src/config.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage'
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FB_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FBAUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FBPROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FBSTORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FB_MESSAGEING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FB_APP_ID
+}
+
+const app = initializeApp(firebaseConfig)
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+})
+const db = getFirestore(app)
+
+export { app, auth, db }
