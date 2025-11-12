08f982af9604f7f3d0ca9031baadb2d74a94be07
tc-kawashima <y.kawashima@timecapsuleinc.co.jp>
2025-11-12 14:45:29 +0900

    Log In & Sign Up

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,16 +1,28 @@
 import {
-    View, Text, TextInput,
+    View, Text, TextInput, Alert,
     TouchableOpacity, StyleSheet
 } from 'react-native'
 
 import { Link, router } from 'expo-router'
 import { useState } from 'react'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 
 import Button from '../../components/Button'
+import { auth } from '../../config'
 
-const handlePress = (): void => {
+const handlePress = (email: string, password: string): void => {
     // 会員登録
-    router.push('/memo/list')
+    console.log(email, password)
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            console.log(userCredential.user.uid)
+            router.replace('/memo/list')
+        })
+        .catch((error) => {
+            const { code, message } = error
+            console.log(code, message)
+            Alert.alert(message)
+        })
 }
 
 const SignUp = () => {
@@ -38,10 +50,10 @@ const SignUp = () => {
                     placeholder='Password'
                     textContentType='password'
                 />
-                <Button label='Submit' onPress={handlePress} />
+                <Button label='Submit' onPress={() => { handlePress(email, password) }} />
                 <View style={styles.footer}>
                     <Text style={styles.footerText}>Already Registered?</Text>
-                    <Link href='/auth/log_in' asChild>
+                    <Link href='/auth/log_in' asChild replace>
                         <TouchableOpacity>
                             <Text style={styles.footerLink}>Log in.</Text>
                         </TouchableOpacity>
